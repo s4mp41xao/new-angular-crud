7ea078df233ba4a45d7bbfc80f2dee7d21fa87bf
s4mp41xao <samuelpaixao@proton.me>
2025-04-23 15:11:04 -0300

    feat: first component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HelloComponent } from './initial/hello/hello.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HelloComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'angular-first-crud';
+
+  name = 'Samuel';
+
+  contador = 0;
+
+  incrementCount() {
+    this.contador++;
+  }
 }
